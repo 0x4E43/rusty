7003305ad26d5e07a836eff450741f3ba507a783
0x4E43 <nimaic.dev@gmail.com>
2023-03-08 11:08:41 +0530

    structures in rust

--- rust_ob/src/main.rs
+++ rust_ob/src/main.rs
@@ -1,7 +1,7 @@
 //for ownership and borrowing
 
 fn main() {
-    let x =10;
+    let x = 10;
     println!("Hello, world!");
     {
         let x = 100;
@@ -9,15 +9,15 @@ fn main() {
     }
     println!("X is {x}");
 
-
     let mut s = String::from("hello");
+    let name = String::from("Nimai");
+    println!("My name is : {}", name);
     s.push_str(" string");
     println!("{}", s);
 
-
     let s1 = String::from("hello");
-    let s2 = s1; 
-    let s3 = s2.clone();  //It can work on String, what about primitive type 
+    let s2 = s1;
+    let s3 = s2.clone(); //It can work on String, what about primitive type
     println!("S2- {}", s2);
     println!("S3- {}", s3);
 
@@ -25,28 +25,28 @@ fn main() {
 
     let x = 5;
     let y = x; // x is not dropped here, why?
-    println!("{} {}", x,y);
+    println!("{} {}", x, y);
 
-    let mut st =  String::from("Nimai");
+    let mut st = String::from("Nimai");
     test(&st);
     println!("After test {st}");
     test_print(&mut st); //as we are playing with refence scope will be there
     println!("After change: {st} Dangle: {}", no_dangle())
 }
 
-fn test(st : &str)-> &str{
+fn test(st: &str) -> &str {
     println!("String {}", st);
     st //this will return st without return statement
 }
 
 //test print function
-fn test_print(st : &mut String){
+fn test_print(st: &mut String) {
     println!("ST: {}", st);
     st.push_str("Charan")
 }
 
 //dangling reference
-fn no_dangle() -> String{
+fn no_dangle() -> String {
     let s = String::from("Hello Rust🦀");
     s
 }
