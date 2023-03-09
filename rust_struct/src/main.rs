48b2d1203836eca49de9847f6d435c8c78841afe
0x4E43 <nimaic.dev@gmail.com>
2023-03-09 11:20:19 +0530

    structures in rust implementation

--- rust_struct/src/main.rs
+++ rust_struct/src/main.rs
@@ -1,3 +1,23 @@
+struct User {
+    name: String,
+    email: String,
+    is_active: bool,
+    login_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user = User {
+        name: String::from("Nimai"),
+        email: String::from("nimaic.dev@gmail.com"),
+        is_active: true,
+        login_count: 54,
+    };
+
+    let user2 = User {
+        name: String::from("Nimai Charan"),
+        ..user //copy other data from the user variable
+    };
+    println!("Name : {}", user.name);
+    println!("Name : {}", user2.name);
+    println!("Name : {}", user2.email);
 }
