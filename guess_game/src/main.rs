46b9c89bf50611f249155cc2679d620bdaa22171
0x4E43 <nimaic.dev@gmail.com>
2024-02-26 23:22:18 +0530

    taking input

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+
+    let mut buf = String::new();
+    // take input
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("hellos");
+
+
+    println!("You entered : {buf}")
+
 }
