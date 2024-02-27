80f55e642ff851d36323c25a907d00c2ff58b6fe
0x4E43 <nimaic.dev@gmail.com>
2024-02-27 23:36:06 +0530

    Guessing number

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,15 +1,25 @@
-use std::io;
+use std::{cmp::Ordering, io};
 
 fn main() {
     println!("Hello, world!");
 
-    let mut buf = String::new();
+    let mut input = String::new();
     // take input
     io::stdin()
-        .read_line(&mut buf)
+        .read_line(&mut input)
         .expect("hellos");
 
+    let num: i32  =  input.trim().parse().expect("not a number");
+    let hard_coded_num = 45;
+    match  num.cmp(&hard_coded_num){
+        Ordering::Less => print_result("Number is less"),
+        Ordering::Equal => print_result("Number is Equal"),
+        Ordering::Greater => print_result("Number is More"),
+    }
 
-    println!("You entered : {buf}")
 
+}
+
+fn print_result(str: &str){
+    println!("{str}")
 }
