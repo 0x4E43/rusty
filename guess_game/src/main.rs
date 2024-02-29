35ac522b3493793f874ec85bb7ee03a35f9f7705
0x4E43 <nimaic.dev@gmail.com>
2024-02-29 23:48:00 +0530

    code cleanup

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -3,6 +3,15 @@ use rand::Rng;
 
 
 fn main() {
+    
+    guess_function();
+
+}
+fn print_result(str: &str){
+    println!("{str}")
+}
+
+fn guess_function(){
     //generate a random number between 0-100
     let hard_coded_num = rand::thread_rng().gen_range(1..=100);
     println!("Hello, world! {hard_coded_num}");
@@ -19,10 +28,6 @@ fn main() {
         Ordering::Equal => print_result("Number is Equal"),
         Ordering::Greater => print_result("Number is More"),
     }
-
-
 }
 
-fn print_result(str: &str){
-    println!("{str}")
-}
+
