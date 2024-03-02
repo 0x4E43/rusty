31f3e305f741b603d5fbb945ee1378c19355c9c4
0x4E43 <nimaic.dev@gmail.com>
2024-03-02 23:50:21 +0530

    using loop

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -3,8 +3,11 @@ use rand::Rng;
 
 
 fn main() {
+    //generate a random number between 0-100
+    let guess_num = rand::thread_rng().gen_range(1..=100);
+    println!("Hello, world! {guess_num}");
     loop {
-        guess_function();
+        guess_function(guess_num);
     }
 
 }
@@ -12,10 +15,7 @@ fn print_result(str: &str){
     println!("{str}")
 }
 
-fn guess_function(){
-    //generate a random number between 0-100
-    let hard_coded_num = rand::thread_rng().gen_range(1..=100);
-    println!("Hello, world! {hard_coded_num}");
+fn guess_function(guess_num :i32){
 
     let mut input = String::new();
     // take input
@@ -24,7 +24,7 @@ fn guess_function(){
         .expect("hellos");
 
     let num: i32  =  input.trim().parse().expect("not a number");
-    match  num.cmp(&hard_coded_num){
+    match  num.cmp(&guess_num){
         Ordering::Less => print_result("Number is less"),
         Ordering::Equal => print_result("Number is Equal"),
         Ordering::Greater => print_result("Number is More"),
