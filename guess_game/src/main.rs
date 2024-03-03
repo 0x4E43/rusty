9b9c1faea981d56eb7b055a0216ba64702439778
0x4E43 <nimaic.dev@gmail.com>
2024-03-03 23:56:40 +0530

    guess game complete

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -6,9 +6,8 @@ fn main() {
     //generate a random number between 0-100
     let guess_num = rand::thread_rng().gen_range(1..=100);
     println!("Hello, world! {guess_num}");
-    loop {
-        guess_function(guess_num);
-    }
+    guess_function(guess_num);
+
 
 }
 fn print_result(str: &str){
@@ -16,18 +15,27 @@ fn print_result(str: &str){
 }
 
 fn guess_function(guess_num :i32){
-
-    let mut input = String::new();
-    // take input
-    io::stdin()
-        .read_line(&mut input)
-        .expect("hellos");
-
-    let num: i32  =  input.trim().parse().expect("not a number");
-    match  num.cmp(&guess_num){
-        Ordering::Less => print_result("Number is less"),
-        Ordering::Equal => print_result("Number is Equal"),
-        Ordering::Greater => print_result("Number is More"),
+    loop {
+        
+        let mut input = String::new();
+        // take input
+        io::stdin()
+            .read_line(&mut input)
+            .expect("hellos");
+    
+        let num: i32  =  match  input.trim().parse(){
+            Ok(n) => n,
+            Err(_) => continue,
+        };
+        
+        match  num.cmp(&guess_num){
+            Ordering::Less => print_result("Number is less"),
+            Ordering::Equal => {
+                print_result("You win!");
+                break;
+            },
+            Ordering::Greater => print_result("Number is More"),
+        }
     }
 }
 
