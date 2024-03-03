150be3ef42e5a8f04ffcf72981cb926dd60c0dab
0x4E43 <nimaic.dev@gmail.com>
2024-03-04 00:21:09 +0530

    Added colors, Finished cargo+rust guessing game

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,22 +1,25 @@
 use std::{cmp::Ordering, io};
 use rand::Rng;
+use colored::*;
 
 
 fn main() {
     //generate a random number between 0-100
+    println!("{}", "Enter a number to guess :".magenta().bold());
     let guess_num = rand::thread_rng().gen_range(1..=100);
     guess_function(guess_num);
 
 
 }
 fn print_result(str: &str, num :i32){
-    println!("{num} is {str}")
+    println!("{} {num} is {str}", "SORRY!".blue())
 }
 
 fn guess_function(guess_num :i32){
     loop {
         
         let mut input = String::new();
+        
         // take input
         match io::stdin()
             .read_line(&mut input) {
@@ -30,14 +33,13 @@ fn guess_function(guess_num :i32){
         let num: i32  =  match  input.trim().parse(){
             Ok(n) => n,
             Err(_) => {
-                println!("Not a number");
+                println!("{}", "Please enter a number".red());
                 continue},
         };
-
         match  num.cmp(&guess_num){
-            Ordering::Less => print_result("Less", guess_num),
+            Ordering::Less => print_result("Less", num),
             Ordering::Equal => {
-                println!("Matched, You Won!");
+                println!("{}", "Matched, You Won!".green());
                 break;
             },
             Ordering::Greater => print_result("More", num),
