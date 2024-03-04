6f1afc954d1dc2398fddd8a9f945767061d155d3
0x4E43 <nimaic.dev@gmail.com>
2024-03-04 23:14:45 +0530

    mutablilty in rust

--- rust-funda/src/main.rs
+++ rust-funda/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x = 5;
+    let mut x = 5; //once it is mutable by declaring as mut value can be reassigned
     println!("x is {x}");
-    x = 6 ; //x cant be assigned as every variable is immutable in rust
+    x = 6 ; //x can be assigned as every variable is mutable defined explicitly
     println!("x is {x}"); 
 }
