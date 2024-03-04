81e85867dd31d4db6c0d42285b80b649178c7913
0x4E43 <nimaic.dev@gmail.com>
2024-03-04 23:19:38 +0530

    constants

--- rust-funda/src/main.rs
+++ rust-funda/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
     println!("x is {x}");
     x = 6 ; //x can be assigned as every variable is mutable defined explicitly
     println!("x is {x}"); 
+
+
+    const HOURS_IN_SECOND :i32 = 60*60; //constants cant be mutable
+    let HOURS_IN_SECONDS = 40; // will throw warning as only constants should be upparcase
+    //and variable should be lowercase
+    println!("CONSTANAT: {HOURS_IN_SECOND}")
 }
