777bc61f331f6fb55b44a8f37b9706c94eaed681
0x4E43 <nimaic.dev@gmail.com>
2024-03-04 23:36:21 +0530

    integer overflow

--- rust-dtype/src/main.rs
+++ rust-dtype/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
-    let guess :i8 = "42".parse().expect("not a number"); //type need to specified while parsing
+    //below will give integer overflow as i8 can have maximum 255
+    let guess :i8 = "300".parse().expect("not a number"); //type need to specified while parsing
     println!("Hello, world! {guess}");
 }
