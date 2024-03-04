1bcf3728d332c93c231083d0e026284031f37483
0x4E43 <nimaic.dev@gmail.com>
2024-03-04 23:20:57 +0530

    not constants

--- rust-funda/src/main.rs
+++ rust-funda/src/main.rs
@@ -6,7 +6,9 @@ fn main() {
 
 
     const HOURS_IN_SECOND :i32 = 60*60; //constants cant be mutable
-    let HOURS_IN_SECONDS = 40; // will throw warning as only constants should be upparcase
+    let hour_in_second  = 60*60; // will throw warning as only constants should be upparcase
     //and variable should be lowercase
-    println!("CONSTANAT: {HOURS_IN_SECOND}")
+    println!("CONSTANAT: {HOURS_IN_SECOND}");
+    println!("NOT CONSTANAT: {hour_in_second}");
+
 }
