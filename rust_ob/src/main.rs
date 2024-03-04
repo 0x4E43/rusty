f861cb104cd53dd0d71668e3a87fb610bd126688
0x4E43 <nimaic.dev@gmail.com>
2024-03-05 00:36:29 +0530

    rust is fucking good

--- rust_ob/src/main.rs
+++ rust_ob/src/main.rs
@@ -7,6 +7,17 @@ fn main() {
         let x = 100;
         println!("X is {x}");
     }
-    println!("X is {x}")
+    println!("X is {x}");
+
+
+    let mut s = String::from("hello");
+    s.push_str(" string");
+    println!("{}", s);
+
+
+    let s1 = String::from("hello");
+    let s2 = s1;    
+
+    println!("{}, world!", s2);
 }
 
