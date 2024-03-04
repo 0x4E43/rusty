321f3315301d3b5479e5ac3ce1c2b3b3cf4dde52
0x4E43 <nimaic.dev@gmail.com>
2024-03-05 00:01:13 +0530

    playing with return type

--- rust_fn/src/main.rs
+++ rust_fn/src/main.rs
@@ -4,5 +4,13 @@ fn main() {
 }
 
 fn test_function(){
-    println!("Test function called")
+    println!("Test function called");
+    let  ll = String::from("hello");
+    let (u, v) = tt(ll);
+    println!("After tt U: {u} V: {v}") ;
+}
+
+fn tt(st :String) -> (String, String) {
+    let st = st+ " hello";
+    return  (st, String::from("nimai"));
 }
